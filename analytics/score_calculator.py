2b3ec88981947c4ecf99bc9dc4725a239bb44d3d
trendmonk <trendmonk@aqua-llama.com>
2025-05-12 15:40:32 +0200

    Add files via upload

---
+++ analytics/score_calculator.py
@@ -0,0 +1,2 @@
+def compute_trend_score(token_id):
+    print(f"Calculating trend score for {token_id}...")
---
+++ analytics/time_decay.py
@@ -0,0 +1,2 @@
+def apply_decay(signal_value, time_delta):
+    print("Applying time decay to signal...")
